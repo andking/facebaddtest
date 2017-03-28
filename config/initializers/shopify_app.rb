ca87fdedd1d1d6dcd961d18a37c161596c65b053
andking <yuschenko_andrey@ukr.net>
2017-03-28 19:29:49 +0300

    make it best

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = "5632ad1a99879991a690beab856510d4"
+  config.secret = "5f16c4b9e306dcb0a501ac2d55f3c8ef"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
