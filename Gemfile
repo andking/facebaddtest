a67dcfd9a7104cba34278953c8e8b473e99a791f
andking <yuschenko_andrey@ukr.net>
2017-03-29 11:28:03 +0300

    make it better

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'shopify_app'
 gem 'activeresource', github: 'rails/activeresource'
+
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,5 +1,5 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
+  config.application_name = "FacebAddTest"
   config.api_key = "5632ad1a99879991a690beab856510d4"
   config.secret = "5f16c4b9e306dcb0a501ac2d55f3c8ef"
   config.scope = "read_orders, read_products"
